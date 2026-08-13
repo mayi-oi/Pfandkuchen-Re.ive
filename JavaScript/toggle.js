8110d03dbcf4ab1d076e711941ccd396ace27cc4
mayi-oi <arkadlen@proton.me>
2026-08-13 13:40:59 +0200

    Showcase mein setup!

--- JavaScript/toggle.js
+++ JavaScript/toggle.js
@@ -1,3 +1,8 @@
+function Setup_bilder() {
+    const el = document.querySelector('.Setup-overlay')
+    if (el) el.classList.toggle('open')
+}
+
 function Menutoggle() {
     const el = document.querySelector('.Hauptmenu-content')
     if (el) el.classList.toggle('open')
